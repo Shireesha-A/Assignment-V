f116e8e2fe033f80edb4bc5d4dad8e5f42cf0519
unknown <shireesha.vani@gmail.com>
2020-08-01 19:42:56 +0530

    second commit

--- React_DOM/my-react-app/src/components/Navbar.js
+++ React_DOM/my-react-app/src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navbar=(props)=>{
     return(
         <nav>
-            <h2 style={{color:"blue",textAlign:"center",textDecoration:"underline"}}>{props.heading}</h2>
+            <h1 style={{color:"blue",textAlign:"center"}}>{props.heading}</h1>
         </nav>
 
     );
